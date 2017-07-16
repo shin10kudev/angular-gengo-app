83431339c7c5a3cc813720e62fb87502fa8126e6
eliotc1986 <hindman@hyakusen.org>
2017-07-16 09:30:05 +0900

    fix reactions

--- src/app/reaction/reaction.component.ts
+++ src/app/reaction/reaction.component.ts
@@ -9,59 +9,51 @@ import * as _ from "lodash";
 })
 export class ReactionComponent implements OnInit, OnDestroy {
 
-  ngOnInit() {
+  @Input() itemId: string;
 
+  showEmojis = false;
+  emojiList: string[];
+
+  reactionCount: any;
+  userReaction: any;
+
+  subscription: any;
+
+  constructor(private reactionSvc: ReactionService) { }
+
+  ngOnInit() {
+    this.emojiList = this.reactionSvc.emojiList
+
+    this.subscription = this.reactionSvc.getReactions(this.itemId)
+                         .subscribe(reactions => {
+
+                           this.reactionCount = this.reactionSvc.countReactions(reactions)
+                           this.userReaction  = this.reactionSvc.userReaction(reactions)
+
+    })
+  }
+
+  react(val) {
+    if (this.userReaction === val) {
+      this.reactionSvc.removeReaction(this.itemId)
+    } else {
+      this.reactionSvc.updateReaction(this.itemId, val)
+    }
+  }
+
+  toggleShow() {
+    this.showEmojis = !this.showEmojis
+  }
+
+  emojiPath(emoji) {
+   return `assets/reactions/${emoji}.svg`
+  }
+
+  hasReactions(index) {
+    return _.get(this.reactionCount, index.toString())
   }
 
   ngOnDestroy() {
-
+    this.subscription.unsubscribe()
   }
-
-  // @Input() itemId: string;
-
-  // showEmojis = false;
-  // emojiList: string[];
-
-  // reactionCount: any;
-  // userReaction: any;
-
-  // subscription: any;
-
-  // constructor(private reactionSvc: ReactionService) { }
-
-  // ngOnInit() {
-  //   this.emojiList = this.reactionSvc.emojiList
-
-  //   this.subscription = this.reactionSvc.getReactions(this.itemId)
-  //                        .subscribe(reactions => {
-
-  //                          this.reactionCount = this.reactionSvc.countReactions(reactions)
-  //                          this.userReaction  = this.reactionSvc.userReaction(reactions)
-
-  //   })
-  // }
-
-  // react(val) {
-  //   if (this.userReaction === val) {
-  //     this.reactionSvc.removeReaction(this.itemId)
-  //   } else {
-  //     this.reactionSvc.updateReaction(this.itemId, val)
-  //   }
-  // }
-
-  // toggleShow() {
-  //   this.showEmojis = !this.showEmojis
-  // }
-
-  // emojiPath(emoji) {
-  //  return `assets/reactions/${emoji}.svg`
-  // }
-
-  // hasReactions(index) {
-  //   return _.get(this.reactionCount, index.toString())
-  // }
-
-  // ngOnDestroy() {
-  //   this.subscription.unsubscribe()
-  // }
 }
