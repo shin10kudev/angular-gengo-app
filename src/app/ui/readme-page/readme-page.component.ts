135527bd97ef2e76083df04525030d4bfaeb9779
eliotc1986 <hindman@hyakusen.org>
2017-08-01 16:36:48 +0900

    fix visibility of signin box

--- src/app/ui/readme-page/readme-page.component.ts
+++ src/app/ui/readme-page/readme-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../core/auth.service";
 
 @Component({
   selector: 'readme-page',
@@ -7,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadmePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
   }
-
 }
