82ecc7296261e7987a18ae6ede302f5fd758ae3b
Elliott Hindman <hindman@hyakuren.org>
2018-11-17 15:06:16 +0900

    add modal for new learning point

--- src/app/mistakes/mistake-form/mistake-form.component.ts
+++ src/app/mistakes/mistake-form/mistake-form.component.ts
@@ -1,26 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MistakeService } from '../shared/mistake.service';
-import { Mistake } from '../shared/mistake';
+import { Component, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { MistakeService } from "../shared/mistake.service";
+import { Mistake } from "../shared/mistake";
 
 @Component({
-  selector: 'mistake-form',
-  templateUrl: './mistake-form.component.html',
-  styleUrls: ['./mistake-form.component.scss']
+  selector: "mistake-form",
+  templateUrl: "./mistake-form.component.html",
+  styleUrls: ["./mistake-form.component.scss"]
 })
-
 export class MistakeFormComponent implements OnInit {
-
   mistake: Mistake = new Mistake();
+  newMistakeModalOpen: boolean = false;
 
-  constructor(private mistakeSvc: MistakeService) { }
+  constructor(private mistakeSvc: MistakeService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  toggleNewMistakeModal() {
+    this.newMistakeModalOpen = !this.newMistakeModalOpen;
   }
 
   createMistake() {
     this.mistakeSvc.createMistake(this.mistake);
     this.mistake = new Mistake(); // reset form
+    this.toggleNewMistakeModal();
   }
 
   cancelMistake() {
